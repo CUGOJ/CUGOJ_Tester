29e2e07c24a66373f2d369c7ce093593fe3a4673
zhaoxiao <504315513@qq.com>
2022-09-19 21:49:31 +0800

    更新tester测试样例

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using CUGOJ.RPC.Gen.Services.Core;
+// See https://aka.ms/new-console-template for more information
 if (args.Length < 1)
     Console.WriteLine("请输入服务地址");
 var buffer = args[0].Split(":");
@@ -6,18 +8,28 @@ if (buffer.Length != 2)
     Console.WriteLine("服务地址格式错误");
 var host = buffer[0];
 var port = int.Parse(buffer[1]);
-using TTransport transport = new TSocketTransport(host, port, new TConfiguration(), 1000);
+var hostByte = System.Text.UTF8Encoding.UTF8.GetBytes(host);
+var ipAddress = System.Net.IPAddress.Parse(host);
+using TTransport transport = new TSocketTransport(ipAddress, port, new TConfiguration(), 1000);
 TProtocol protocol = new TBinaryProtocol(transport);
-var client = new CUGOJ.RPC.Gen.Services.Base.BaseService.Client(protocol);
+await transport.OpenAsync();
+
+var client = new CUGOJ.RPC.Gen.Services.Core.CoreService.Client(protocol);
 
 try
 {//TODO 写自己的测试代码
-    Console.WriteLine((await client.MulGetProblemInfo(new CUGOJ.RPC.Gen.Services.Base.MulGetProblemInfoRequest()
-    {
-        ProblemIDList = new List<long>() { 1, 2 },
-        IsGetProblemContent = true,
-        Base = RPCTools.NewBase()
-    })));
+    using var activity = new Activity("CUGOJ.Tester");
+    activity.Start();
+    // var req = new PingRequest(((long)(DateTime.Now - DateTime.UnixEpoch).TotalMilliseconds));
+    // var req = new RestartRequest();
+    var req = new AddServiceRequest(ServiceTypeEnum.Base);
+    // var req = new GetUnRegisteredServicesRequest();
+    req.Base = RPCTools.NewRootBase();
+    req.Base.Extra["ServiceID"] = "testService";
+    req.Base.Extra["UserID"] = "Tester";
+    Console.WriteLine((await client.AddService(req)));
+    // Console.WriteLine((await client.Restart(req)));
+    // Console.WriteLine(await client.GetUnRegisteredServices(req));
 }
 catch (Exception e)
 {
